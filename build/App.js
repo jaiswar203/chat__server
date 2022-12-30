353436b5891bcfd442522768e9874c6ded7b35b5
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-12-30 18:55:10 +0530

    added

--- build/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Server = /** @class */ (function () {
-    function Server(port, public) {
-        this.port = port;
-    }
-    return Server;
-}());
--- build/library/resonse.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Packet = void 0;
-function Packet(message, data) {
-    return { message, data };
-}
-exports.Packet = Packet;
